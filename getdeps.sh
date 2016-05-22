f08a4e5c9fa98dfc2279150ba717f0da9552eec5
Guohan Lu <gulv@microsoft.com>
2016-05-22 03:59:53 +0000

    add p4-hlir dependency

--- getdeps.sh
+++ getdeps.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/p4lang/p4-hlir.git
 git clone https://github.com/krambn/behavioral-model p4-bmv2
 git clone https://github.com/krambn/p4c-bm p4c-bmv2
 git clone https://github.com/krambn/switch
@@ -7,4 +8,3 @@ cd ../p4c-bmv2
 git submodule update --init --recursive
 cd ../switch
 git submodule update --init --recursive
-
