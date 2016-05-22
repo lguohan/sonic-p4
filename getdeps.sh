17d43aa7abd579e140813628639f873ebf9b71dc
Guohan Lu <gulv@microsoft.com>
2016-05-22 05:11:05 +0000

    use latest SAI headers

--- getdeps.sh
+++ getdeps.sh
@@ -2,9 +2,13 @@ git clone https://github.com/p4lang/p4-hlir.git
 git clone https://github.com/krambn/behavioral-model p4-bmv2
 git clone https://github.com/krambn/p4c-bm p4c-bmv2
 git clone https://github.com/krambn/switch
+git clone https://github.com/krambn/SAI.git
 cd p4-bmv2
 git submodule update --init --recursive
 cd ../p4c-bmv2
 git submodule update --init --recursive
 cd ../switch
 git submodule update --init --recursive
+cd ../
+mv switch/switchsai/submodules/{SAI,SAI.old}
+cp -r SAI switch/switchsai/submodules/
